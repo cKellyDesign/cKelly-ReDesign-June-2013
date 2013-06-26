51fcc5b26eaf638f78443c61bd6fda24d36f2a90
Conor Kelly <ckelly@ckellydesign.net>
2013-06-25 22:28:01 -0700

    Design Prep and Copy Write
    Still needs finalized pitch

--- desPrep/scripts/cKDjs.js
+++ desPrep/scripts/cKDjs.js
@@ -1,5 +1,6 @@
 var i = 0;
 $(document).ready(function(){
+	var topDist = $(document).scrollTop();
 	if ( topDist >= 536 ) {
 		$('div#deskNav').addClass("stickToTop");
 	} else {
@@ -8,9 +9,14 @@ $(document).ready(function(){
 });
 $(document).scroll(function() {
 	i++;
+	var topDist = $(document).scrollTop();
 	if ( topDist >= 536 ) {
 		$('div#deskNav').addClass("stickToTop");
+	//	$('div#deskNav > nav > ul > li').addClass('four').addClass('columns').removeClass('one-third').removeClass('column');
+	//	$('div#deskNav > nav > ul > li:first-child').addClass('offset-by-four');
 	} else {
 		$('div#deskNav').removeClass("stickToTop");
+	//	$('div#deskNav > nav > ul > li').removeClass('four').removeClass('columns').addClass('one-third').addClass('column').removeClass('offset-by-four');
+		
 	}
 });
