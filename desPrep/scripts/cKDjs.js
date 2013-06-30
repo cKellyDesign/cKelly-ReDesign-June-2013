9d553e56fed6f56a464fea50d915bbc1c5d755ec
Conor Kelly <ckelly@ckellydesign.net>
2013-06-29 18:39:37 -0700

    Plugins Added - Fancybox and Flexslider

--- desPrep/scripts/cKDjs.js
+++ desPrep/scripts/cKDjs.js
@@ -3,29 +3,46 @@ $(document).ready(function(){
 	var topDist = $(document).scrollTop();
 	if ( topDist >= 536 ) {
 		$('div#deskNav').addClass("stickToTop");
-	//	$('section#header-container.container div#deskNav li.one-third.column').addClass("now");
 	} else {
 		$('div#deskNav').removeClass("stickToTop");
-	//	$('section#header-container.container div#deskNav li.one-third.column').removeClass("now");
 	}
+	// ***** Fancy Trigger *****
+	
+	$("a.galThumb").fancybox({
+		
+		fitToView	: false,
+		width		: '85%',
+		height		: '100%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+	
+	// ***** Gallery thumb click *****
+	
+	$('.fancy-gallery-filmstrip ul.thumbs li a').bind({click: function(){
+	/*	
+		if ( $(this).hasClass("current") ) {
+			$(this).addClass("super");
+		} else {
+			$('.current').removeClass("current");
+			$(this).addClass('current');
+			$('.super').removeClass("super");
+		}
+		return false;
+	*/	
+	}	/* end of function */
+	}); /* click bind */
+	
+	
 });
 $(document).scroll(function() {
 	i++;
 	var topDist = $(document).scrollTop();
 	if ( topDist >= 536 ) {
 		$('div#deskNav').addClass("stickToTop");
-	//	$('section#header-container.container div#deskNav li.one-third.column.alpha').addClass("offset-by-four");
-	//	$('section#header-container.container div#deskNav li.one-third.column').addClass("now");
-	
-	//	$('div#deskNav > nav > ul > li').css('width', '220px');
-	//	$('div#deskNav > nav > ul > li').addClass('four').addClass('columns').removeClass('one-third').removeClass('column');
-	//	$('div#deskNav > nav > ul > li:first-child').addClass('offset-by-four');
 	} else {
 		$('div#deskNav').removeClass("stickToTop");
-	//	$('section#header-container.container div#deskNav li.one-third.column.alpha').removeClass("offset-by-four");
-	//	$('section#header-container.container div#deskNav li.one-third.column').removeClass("now");
-	//	$('div#deskNav > nav > ul > li').attr('style', '');
-	//	$('div#deskNav > nav > ul > li').removeClass('four').removeClass('columns').addClass('one-third').addClass('column').removeClass('offset-by-four');
-		
 	}
 });
