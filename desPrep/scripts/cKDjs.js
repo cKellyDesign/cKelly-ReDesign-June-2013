43681172ea3ec6aaaee450456e5c82298dfd8c43
Conor Kelly <ckelly@ckellydesign.net>
2013-07-06 17:28:56 -0700

    Progress Commit

--- desPrep/scripts/cKDjs.js
+++ desPrep/scripts/cKDjs.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	$("a.galThumb").fancybox({
 		
 		fitToView	: false,
-		width		: '85%',
+		width		: '95%',
 		height		: '100%',
 		autoSize	: false,
 		closeClick	: false,
@@ -19,19 +19,60 @@ $(document).ready(function(){
 		closeEffect	: 'none'
 	});
 	
+	// ***** Fancy Trigger *****
+	
+	
+	
+	//$('article.fancy-article#water1stOrg').flexslider();
+	
+  
+	
+	
 	// ***** Gallery thumb click *****
 	
-	$('.fancy-gallery-filmstrip ul.thumbs li a').bind({click: function(){
-	/*	
+	$('.fancy-gallery-filmstrip ol.thumbs li').bind({click: function(){
+	/*	var par = $(this).parents('div.fancy-gallery-filmstrip');
+		if ( $(par).hasClass("collapsed") ) {
+			
+			$(this).parents('div.fancy-gallery-filmstrip').removeClass("collapsed");
+			$(this).parents('article.fancy-article').children('span.area').slideToggle();
+			if ( $(this).hasClass("current") ) {
+			//	var fancyPar = $(this).parents('article.fancy-article').attr('id');
+    		//	$('article.fancy-article#' + fancyPar + ' div.gallery-plugin-container').flexslider();
+			}
+			
+			
+			//alert("detected");
+		} else {
+			
+			if ( $(this).hasClass("current") ) {
+				$(this).parents('div.fancy-gallery-filmstrip').addClass("collapsed");
+				$(this).parents('article.fancy-article').children('span.area').slideToggle();
+			} else {
+				$('.current').removeClass("current");
+				$(this).addClass('current');
+				
+			//	var fancyPar = $(this).parents('article.fancy-article').attr('id');
+    		//	$('article.fancy-article#' + fancyPar + ' div.gallery-plugin-container').flexslider();
+			}
+			
+			
+			//alert("failed");
+		}
+	*/	
+		/*
 		if ( $(this).hasClass("current") ) {
-			$(this).addClass("super");
+		//	$(this).addClass("super");
 		} else {
 			$('.current').removeClass("current");
 			$(this).addClass('current');
-			$('.super').removeClass("super");
+			//$('.super').removeClass("super");
 		}
-		return false;
-	*/	
+		*/
+	
+	
+	
+	
 	}	/* end of function */
 	}); /* click bind */
 	
